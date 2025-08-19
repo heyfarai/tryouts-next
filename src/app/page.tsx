0f6c024f286b1e734a429f1ad1bfc1944c3f89e6
Farai Madzima <farai@icloud.com>
2025-08-18 22:14:54 -0400

    added NCHC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,10 +45,23 @@ export default function Home() {
             <h1 className="alexandria font-extrabold text-4xl lg:text-5xl md:text-4xl mt-0 mb-12 md:leading-[3.2rem]">
               U14 Boys Basketball.
             </h1>
-            <p className="text-xl mb-8">
+            <p className="text-xl mb-8 font-semibold">
               An Ottawa-based U14 AAA+ (Major Bantam) basketball team. <br />{" "}
-              Play for a team that coaches and trains competitive basketball
-              players to elite levels of individual and team skills.
+              For the 2025-26 season we are playing in the highly competitive
+              Ascent League of the{" "}
+              <a
+                className="text-[var(--precision-red)] hover:underline transition"
+                href="https://natcaphoops.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                National Capital Hoops Circuit
+              </a>
+              . <br />
+            </p>
+            <p className="mb-8">
+              Join us and play for a team that coaches and trains competitive
+              basketball players to elite levels of individual and team skills.
             </p>
             <p className="text-lg mb-20 mt-0">
               <a
@@ -60,34 +73,57 @@ export default function Home() {
             </p>
           </div>
           <h3 className="font-bold text-2xl mt-4 mb-2">
-            Leagues, tournaments, and travel
+            League: National Capital Hoops Circuit
           </h3>
           <p className="text-lg mb-12">
-            Games and leagues are TBC. <br /> We expect to travel to highly
-            competitive tournaments across Ontario and Quebec.
+            <span className="font-bold">Games:</span> All 2025-26 league games
+            are in Ottawa. <br />
+            <span className="font-bold">Tournaments:</span> We'll travel to
+            select competitive tournaments across Ontario and Quebec. We plan to
+            minimize travel in our first season.
           </p>
+          <h3 className="font-bold text-2xl mt-4 mb-4">
+            Practice: 3 times a week.
+          </h3>
+          <h3 className="font-bold text-sm uppercase mt-6 mb-2">
+            Practice Days
+          </h3>
+          <p className="text-lg mb-4">
+            Wednesday, then Saturday and Sunday. (2 hours each)
+          </p>
+          <h3 className="font-bold text-sm uppercase mt-6 mb-2">
+            Professional Development
+          </h3>
+          <p className="text-lg mb-4">
+            More than playing with the team, we have a consultant professional
+            development coach to improve <i>YOUR</i> individual game.
+          </p>
+
+          <h3 className="font-bold text-sm uppercase mt-6 mb-2">
+            Practice Locations
+          </h3>
+          <ul className="mb-12">
+            <li>
+              <span className="font-bold">
+                Sir Wilfrid Laurier High School:
+              </span>{" "}
+              1515 Tenth Line Rd, Orléans
+            </li>
+            <li>
+              <span className="font-bold">BGC Taggart Parkes:</span> 1770
+              Heatherington Rd.
+            </li>
+            <li>
+              <span className="font-bold">BGC Tomlinson:</span> 1463 Prince of
+              Wales Dr.
+            </li>
+          </ul>
           <h3 className="font-bold text-2xl mt-4 mb-2">What we believe</h3>
           <p className="text-lg mb-12">
             Hard work. No cap. No yap. <br />
             Respect. For ourselves. For those around us. <br />
             Boosting self-esteem and confidence.
           </p>
-          <h3 className="font-bold text-2xl mt-4 mb-2">
-            We practice 3 times a week.
-          </h3>
-          <p className="text-lg mb-4">
-            1 weeknight, then Saturday and Sunday. (2 hours each)
-            <br />
-            Gamedays and Leagues are TBC.
-          </p>
-
-          <h3 className="font-bold text-sm uppercase mt-0 mb-2">
-            Practice Locations
-          </h3>
-          <ul className="mb-12">
-            <li>BGC Taggart Parkes: 1770 Heatherington Rd.</li>
-            <li>BGC Tomlinson: 1463 Prince of Wales Dr.</li>
-          </ul>
         </div>
       </div>
       <div
